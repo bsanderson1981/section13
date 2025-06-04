b6d22130dde4fa562689ee6405e637b55a4e1bfc
Bill Sanderson <bill@towniebagels.com>
2025-06-04 10:06:08 -0700

    start section 13 - corrected code base for Flatbutton to Textbutton and RaisedButton with ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:section13/screens/loading_screen.dart';
-
+//git init && git add . && git commit -m "Initial commit" && git remote add origin https://github.com/bsanderson1981/section8 && git branch -M main && git push -u origin main
+//start  of section 13  corrected code base
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
