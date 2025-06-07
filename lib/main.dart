9b2fe9d0aaf58a9a2db11ca38e22fe15463c614a
Bill Sanderson <bill@towniebagels.com>
2025-06-07 13:40:31 -0700

    start section 13 - finish lesson #146

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:section13/screens/loading_screen.dart';
 //git init && git add . && git commit -m "Initial commit" && git remote add origin https://github.com/bsanderson1981/section8 && git branch -M main && git push -u origin main
-//finished  of section 13  #144
+//finished  of section 13  #146
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import '/services/location.dart';
-//start section 13   142
+import 'package:http/http.dart' as http;
+
 class LoadingScreen extends StatefulWidget {
-
   @override
-
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -19,15 +17,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    final double lat = location.latitude;
+    final double lon = location.longitude;
+    print('Latitude: $lat');
+    print('Longitude: $lon');
 
+    getData(lat, lon); // Now fetch weather data using coordinates
   }
 
+  void getData(double lat, double lon) async {
+    http.Response  response = await http.get(
+      Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=50836a4419e6435e0b1f803d6a434a23'),
+    );
+
+    if (response.statusCode == 200) {
+      print(response.body); // raw JSON output
+      print('RCode: ${response.statusCode}');
+    } else {
+      print('Failed to get weather data: ${response.statusCode}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(child: Text('Loading...')),
     );
   }
 }
